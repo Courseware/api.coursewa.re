27658f4608af8178ab0f8e2c0c7ba13b78f7dbff
Stas SUȘCOV <stas@net.utcluj.ro>
2013-03-19 21:22:52 +0200

    Load doorkeeper routes inside app not engine.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Coursewareable::Engine.routes.draw do
-  use_doorkeeper do
-    # can be :authorizations, :tokens, :applications, :authorized_applications
-    skip_controllers :applications, :authorized_applications
-  end
-
   scope :module => :api do
     namespace :v1 do
       resources :users, :only => [] do
@@ -16,6 +11,11 @@ end
 
 CoursewareAPI::Application.routes.draw do
   # Route overwrites come below:
+  use_doorkeeper do
+    # can be :authorizations, :tokens, :applications, :authorized_applications
+    skip_controllers :applications, :authorized_applications
+  end
+
 
   # Mount the Coursewareable Engine
   mount Coursewareable::Engine => '/'
