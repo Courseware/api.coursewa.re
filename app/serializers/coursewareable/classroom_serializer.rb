de605535f24d2f23cb89a39efea85da39e77f248
Calin Salagean <salagean.calin@gmail.com>
2013-03-24 17:44:03 +0200

    add collaboration in classroom serializer

--- app/serializers/coursewareable/classroom_serializer.rb
+++ app/serializers/coursewareable/classroom_serializer.rb
@@ -1,4 +1,5 @@
 # [Coursewareable::Classroom] serializer class
 class Coursewareable::ClassroomSerializer < ActiveModel::Serializer
   attributes :id, :description, :title, :owner
+  has_many :collaborations
 end
