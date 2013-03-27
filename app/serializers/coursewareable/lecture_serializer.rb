f1ae8bc9e719388c29105b81b5f1f0bce1b1ac88
Calin Salagean <salagean.calin@gmail.com>
2013-03-27 19:29:07 +0200

    specify module for Lecture serializer @wip

--- app/serializers/coursewareable/lecture_serializer.rb
+++ app/serializers/coursewareable/lecture_serializer.rb
@@ -1,3 +1,4 @@
-class LectureSerializer < ActiveModel::Serializer
+# [Coursewareable::Lecture] serializer class
+class Coursewareable::LectureSerializer < ActiveModel::Serializer
   attributes :id, :slug, :title, :content, :requisite
 end
