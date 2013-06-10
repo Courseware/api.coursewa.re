cac6da55da57e9851dc3b7c89469686b21ba834e
Stas SUȘCOV <stas@net.utcluj.ro>
2013-06-10 17:42:39 +0300

    Updated app fabricator to have an owner always.

--- spec/fabricators/doorkeeper/application_fabricator.rb
+++ spec/fabricators/doorkeeper/application_fabricator.rb
@@ -1,4 +1,5 @@
 Fabricator(:doorkeeper_app, :class_name => ::Doorkeeper::Application) do
+  owner(:fabricator => 'coursewareable/user')
   name          { sequence(:app_id){ Faker::Company.name + ' App' } }
   redirect_uri  { 'http://%s/callback' % Faker::Internet.domain_name }
 end
